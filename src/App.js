b5128dede52b0c2ede64d7f431850d8bd0b422e6
Omojiba <tolu.omojiba@gmail.com>
2022-06-03 12:24:18 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+
+import ProgressCircle from './components/progress_circle';
+import './test.css';
+import facebook from './components/Assets/facebook.png';
+import twitter from './components/Assets/twitter.png';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <section className='left'></section>
+
+         <section className='middle'>
+            <h2>May 2022 Performance</h2>
+
+               <div className='middle_grid_one'>
+
+                   <div className='box views_box'>
+                      <h2>VIEWS</h2>
+                      <h3>7,124,568</h3>
+                   </div>
+
+                   <div className=' box follows_box'>
+                      <h2>FOLLOWS</h2>
+                      <h3>91,344</h3>
+                   </div>
+
+                  <div className='box likes_box'>
+                     <h2>LIKES</h2>
+                     <h3>654,931</h3>
+                  </div>
+               </div>
+
+          <h2>Statistics</h2>
+          <div className='weekly_target_gridbox'>
+
+            <div className='target weekly_target_one'>
+              <div className='target_textbox'>
+                <h2>Weekly Target</h2>
+                <h3>25% Achieved</h3>
+              </div>
+              <ProgressCircle/>
+            </div>
+            <div className='target weekly_target_two'></div>
+          </div>
+
+          <h2>Monthly Tasks</h2>
+          <div className='tasks_container'>
+
+            <div className='task_flexbox'>
+              <img src={facebook} alt='facebook_icon'/>
+              <div className='task_description'>
+                <h2>Facebook Ads</h2>
+                <p>Gain atleast 50 visits through facebook ads</p>
+              </div>
+            </div>
+
+            <div className='task_flexbox'>
+              <img src={facebook} alt='facebook_icon'/>
+              <div className='task_description'>
+                <h2>Facebook Ads</h2>
+                <p>Gain atleast 50 visits through facebook ads</p>
+              </div>
+            </div>
+
+            <div className='task_flexbox'>
+              <img src={facebook} alt='facebook_icon'/>
+              <div className='task_description'>
+                <h2>Facebook Ads</h2>
+                <p>Gain atleast 50 visits through facebook ads</p>
+              </div>
+            </div>
+
+            <div className='task_flexbox'>
+              <img src={facebook} alt='facebook_icon'/>
+              <div className='task_description'>
+                <h2>Facebook Ads</h2>
+                <p>Gain atleast 50 visits through facebook ads</p>
+              </div>
+            </div>
+          </div>
+
+
+        </section>
+        <section className='right'></section>
+
+  
     </div>
   );
 }
---
+++ src/components/progress_circle.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './circle.css'
+
+function ProgressCircle() {
+
+    let number = document.getElementById('number');
+    let counter = 0;
+    setInterval(() => {
+        if(counter == 25){clearInterval();
+        }
+        else{
+        counter += 1;
+        number.innerHTML = counter + '%';}
+
+    }, 30);
+  return (
+      <div className='container'>
+         <div className='progress_circle'>
+                <div className='outer_circle'>
+                  <div className='inner_circle'>
+                      <h3 id='number'></h3>
+                  </div>
+                </div>
+
+                <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="160px" height="160px">
+         <circle cx="45" cy="45" r="38" stroke-linecap="round" />
+ </svg>
+        </div>
+
+        <script>
+            
+        </script>
+     </div>
+  )
+}
+
+export default ProgressCircle
